cb34532dde77619ed4223d1f45bd1ee1568cc360
HafsaA85 <hafsaadeel4@gmail.com>
2025-10-01 12:34:43 +0100

    Login form, notification code, images for README

--- clients/models.py
+++ clients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,3 +13,12 @@ class Client(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+    class Notification(models.Model):
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        message = models.TextField()
+        read = models.BooleanField(default=False)
+        timestamp = models.DateTimeField(auto_now_add=True)
+    
+        def __str__(self):
+            return f"Notification for {self.user.username}: {self.message}"
--- clients/urls.py
+++ clients/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.client_signup, name='client_signup'),
     path('signup/success/', views.signup_success, name='signup_success'),
+    path('notifications/', views.notifications_view, name='notifications'),
+    path('login/', views.client_login, name='client_login'),
     
 ]
